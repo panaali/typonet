882d11f1be6a9edc8e742de2f59081614c620758
Ali akbar panahi <panaali@gmail.com>
2012-12-13 03:18:57 +0330

    aa

--- js/client.js
+++ js/client.js
@@ -10,11 +10,16 @@ App.Views = App.Views || {};
 App.Login = Backbone.View.extend({
 	events : {
 		'click button' : 'signin',
+		'click .login' : 'signin',
+		'click body' : 'signin',
+		'click body' : 'signin',
+		'click body' : 'signin',
 	},
 	initialize: function()
     {
 		$('.input-block-level').focus();
 		_.bindAll(this, 'render' , 'signin');
+		
 	},
 	render : function(){
 		console.log('aaa');
