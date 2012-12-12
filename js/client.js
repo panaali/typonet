62829f9432a0240cfa4137fc7cc917a4e8b0c00e
Ali akbar panahi <panaali@gmail.com>
2012-12-13 03:02:45 +0330

    backbone!

---
+++ js/client.js
@@ -0,0 +1,33 @@
+/*!
+* Client.js by @aliakbarpanhi & @aghasoroush
+* Copyright 2012 HyperTeanab
+* http://www.apache.org/licenses/LICENSE-2.0.txt
+*/
+;
+var App = App || {};
+App.Views = App.Views || {};
+
+App.Login = Backbone.View.extend({
+	events : {
+		'click button' : 'signin',
+	},
+	initialize: function()
+    {
+		$('.input-block-level').focus();
+		_.bindAll(this, 'render' , 'signin');
+	},
+	render : function(){
+		console.log('aaa');
+		
+	},
+	signin: function() {
+		// Socket staf for login ...
+		console.log('bbb');
+		$('.form-signin').remove();
+		return false;
+	}
+});
+
+$(document).ready(function () {
+	obj = new App.Login();
+});
