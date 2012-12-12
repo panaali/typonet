bbc8e759091621fc3262bd7204b9cff48db69a46
Ali akbar panahi <panaali@gmail.com>
2012-12-13 03:23:22 +0330

    aa

--- js/client.js
+++ js/client.js
@@ -9,30 +9,23 @@ App.Views = App.Views || {};
 
 App.Login = Backbone.View.extend({
 	events : {
-		'click button' : 'signin',
 		'click .login' : 'signin',
-		'click body' : 'signin',
-		'click body' : 'signin',
-		'click body' : 'signin',
 	},
 	initialize: function()
     {
 		$('.input-block-level').focus();
 		_.bindAll(this, 'render' , 'signin');
-		
 	},
 	render : function(){
-		console.log('aaa');
 		
 	},
 	signin: function() {
 		// Socket staf for login ...
-		console.log('bbb');
 		$('.form-signin').remove();
 		return false;
 	}
 });
 
 $(document).ready(function () {
-	obj = new App.Login();
+	obj = new App.Login({el:$('.container')});
 });
